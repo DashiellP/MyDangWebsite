e8c646923845af2aca31f1d3f331709f4fd91f02
Dashiell Powers <dashiellp@gmail.com>
2018-10-26 18:20:29 -0700

    more header changes

--- Source.js
+++ Source.js
@@ -1,11 +1,3 @@
 function what(id) {
 	document.getElementById(id).innerText = "what";
 }
-
-function setColor(id, color, value) {
-	document.getElementById(id).style.color[0] = value;
-}
-
-function testObj(x) {
-	this.innerText = x;
-}
