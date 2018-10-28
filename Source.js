541d9caacf219154f11c2be3e7e9bd045fb27e16
Dashiell Powers <dashiellp@gmail.com>
2018-10-28 15:45:24 -0700

    Added resize effect to banner

--- Source.js
+++ Source.js
@@ -1,3 +1,11 @@
-function makeSticky(id) {
-	document.getElementById(id).style.position = "sticky";
+function setBannerMargin() {
+    var img = document.getElementById("bannerImg");
+    var header = document.getElementById("headerContainer");
+    console.log(img.clientHeight);
+    console.log(header.clientHeight);
+    header.style.marginTop = img.clientHeight - header.clientHeight;
+}
+
+window.onresize = function () {
+    setBannerMargin();
 }
